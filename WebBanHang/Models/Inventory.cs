20325eb1f25a8b862c50e0aaacce51d3bc102dcf
khang0710 <117827755+khang0710@users.noreply.github.com>
2024-11-27 17:06:39 +0700

    chinhsuaModel

---
+++ WebBanHang/Models/Inventory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebBanHang.Models;
+
+public partial class Inventory
+{
+    public int InventoryId { get; set; }
+
+    public int ProductId { get; set; }
+
+    public string? WarehouseLocation { get; set; }
+
+    public int Quantity { get; set; }
+
+    public DateTime? LastUpdated { get; set; }
+
+    public virtual Product Product { get; set; } = null!;
+}
--- WebBanHang/obj/Debug/net8.0/WebBanHang.AssemblyInfo.cs
+++ WebBanHang/obj/Debug/net8.0/WebBanHang.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBanHang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19896e06eb3a305f7aa2e26b06d8db3b4c2b07ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac22f6dc7e89aa054bd867d3f56a8f6c6fefcc77")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBanHang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBanHang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
